3de6312b79e69beaadbbefd1f4ddefaa08b3498f
at621 <67857437+at621@users.noreply.github.com>
2023-08-26 07:32:23 +0200

    Update convert_doc.py

--- convert_doc.py
+++ convert_doc.py
@@ -16,43 +16,38 @@ def extract_data(text):
     data = []
     level_1_label, level_2_label, level_3_label, level_4_label = None, None, None, None
     body_buffer = []
+    special = False
+    collecting_special_paragraph = False
 
     for i, line in enumerate(lines):
         stripped_line = line.strip()
 
+        # Start collecting for a special paragraph
+        if stripped_line.startswith("&&"):
+            collecting_special_paragraph = True
+            stripped_line = stripped_line[2:].strip()
+
+        # End collecting for a special paragraph
+        if stripped_line.endswith("^^"):
+            collecting_special_paragraph = False
+            stripped_line = stripped_line[:-2].strip()
+            special = True
+
+        # Append to the buffer and continue if we're collecting lines for a special paragraph
+        if collecting_special_paragraph or special:
+            body_buffer.append(stripped_line)
+            if not collecting_special_paragraph:
+                data.append([level_1_label, level_2_label, level_3_label, level_4_label, ' '.join(body_buffer), special])
+                body_buffer = []
+                special = False
+            continue
+
         # Empty line indicates end of a paragraph
         if not stripped_line and body_buffer:
-            data.append([level_1_label, level_2_label, level_3_label, level_4_label, ' '.join(body_buffer)])
+            data.append([level_1_label, level_2_label, level_3_label, level_4_label, ' '.join(body_buffer), special])
             body_buffer = []
             continue
 
         # Detect headings based on our markers
         if stripped_line.startswith("######"):
-            level_4_label = stripped_line.replace("######", "").strip()
-        elif stripped_line.startswith("#####"):
-            level_3_label = stripped_line.replace("#####", "").strip()
-            level_4_label = None
-        elif stripped_line.startswith("####"):
-            level_2_label = stripped_line.replace("####", "").strip()
-            level_3_label, level_4_label = None, None
-        elif stripped_line.startswith("###"):
-            level_1_label = stripped_line.replace("###", "").strip()
-            level_2_label, level_3_label, level_4_label = None, None, None
-        else:
-            body_buffer.append(stripped_line)
-
-    # Add the last buffered body text if present
-    if body_buffer:
-        data.append([level_1_label, level_2_label, level_3_label, level_4_label, ' '.join(body_buffer)])
-
-    return data
-
-pdf_path = "path_to_your_pdf_file.pdf"
-text = convert_pdf_to_text(pdf_path)
-data = extract_data(text)
-
-# Convert data to Pandas DataFrame
-df = pd.DataFrame(data, columns=['Level_1_Label', 'Level_2_Label', 'Level_3_Label', 'Level_4_Label', 'Body'])
-
-# Save the DataFrame to a CSV file if needed
-df.to_csv('output.csv', index=False)
+            level_4_label = stripped_line.replace("######",
